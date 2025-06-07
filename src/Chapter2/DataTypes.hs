8bb38cf60f239cb9f678e9fde755389d3405910f
Elisha Ukpong <ishukpong418@gmail.com>
2025-06-07 01:06:16 +0100

    learning ongoing

--- src/Chapter2/DataTypes.hs
+++ src/Chapter2/DataTypes.hs
@@ -1,6 +1,26 @@
 data Client = GovOrg     String
             | Company    String Integer String String
-            | Individual String String Gender Bool
+            | Individual Person Bool
             deriving Show
 
+data Person = Person String String Gender
+    deriving Show
+
 data Gender = Male | Female | Unknown deriving Show
+
+clientName :: Client -> String
+clientName client = case client of
+                      GovOrg  name                -> name
+                      Company name id person resp -> name
+                      Individual person ads       ->
+                          case person of Person fName lName gender -> fName ++ " " ++ lName
+
+
+countGenders :: [Client] -> GenderCount
+countGenders [] = GenderCount 0 0 0
+countGenders (client:rest) =
+  let GenderCount m f o = countGenders rest
+  in case gender client of
+       Male -> GenderCount (m + 1) f o
+       Female -> GenderCount m (f + 1) o
+       Other -> GenderCount m f (o + 1)
--- src/Chapter2/simpleFunctions.hs
+++ src/Chapter2/simpleFunctions.hs
@@ -1,3 +1,5 @@
+module Chapter2.SimpleFunctions where
+
 firstOrEmpty :: [[Char]] -> [Char]
 firstOrEmpty characterList = if not (null characterList) then head characterList else "empty"
 
@@ -8,3 +10,15 @@ listOne +++ listTwo = (head listOne) : ((tail listOne) +++ listTwo)
 reverse2 :: [a] -> [a]
 reverse2 [] = []
 reverse2 listToReverse = (reverse2 (tail listToReverse)) ++ [head listToReverse]
+
+--filter even [1,2,3,4,5,6,7,8,9]
+
+filterOne :: (Eq a, Num a) => a -> Bool
+filterOne = \y -> y == 1
+
+filterNumber :: (Eq a) => a -> a -> Bool
+filterNumber y = \x -> x == y
+
+filterNot y = \x -> x /= y
+
+
