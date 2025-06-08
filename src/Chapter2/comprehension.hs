38221b55a093a535d4fef6ff3ce95a1184ecc43d
Elisha Ukpong <ishukpong418@gmail.com>
2025-06-08 20:21:55 +0100

    list comprehension

--- src/Chapter2/comprehension.hs
+++ src/Chapter2/comprehension.hs
@@ -1,3 +1,7 @@
 module Comprehension where
 
-items xs = [x^2 | x <- xs, x > 5]
+raiseToPower xs = [x^2 | x <- xs, x > 5]
+
+crossMultiply xs ys = [x*y | x <- xs, y <- ys]
+
+removeLowerCases xs = [x | x <- xs, x `elem` ['A'..'Z']]
