b63f16961f3fe5a29c64cd747a042d9f2257b7ac
Elisha Ukpong <ishukpong418@gmail.com>
2025-06-08 22:39:49 +0100

    fixing

--- src/Chapter2/zip.hs
+++ src/Chapter2/zip.hs
@@ -3,3 +3,11 @@ module Zip where
 zipper [] [] = []
 zipper (x:xs) (y:ys) = [x , y] : zipper xs ys
 zipper' (x:xs) (y:ys) =( x : y : []) : zipper xs ys
+
+-- Using List Comprehension
+
+--zipComprehension first last = [x : y : [] | x <- first, y <- last, zipComprehension (tail first) (tail last)]
+
+firstLetter :: String-> String
+firstLetter "" = "Empty string, whoops!"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
