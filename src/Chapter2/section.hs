50b2cd28c9b75fdc0621468bf0ba7640cf493bfb
Elisha Ukpong <ishukpong418@gmail.com>
2025-06-12 01:09:59 +0100

    working on scans

--- src/Chapter2/section.hs
+++ src/Chapter2/section.hs
@@ -8,3 +8,9 @@ funct f x = f x
 repCom xs = [replicate 3 x | x <- xs]
 
 powerFst xs = [ x | (x,_) <- xs]
+
+powerSqr xs = [[ x ^ 2 | x <- x] | x <- xs]
+
+padding _ 0 _ = []
+padding xs n v = xs : v
+--padding xs n v = padding (xs ++ [v]) (n-1) v
