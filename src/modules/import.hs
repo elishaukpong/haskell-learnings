92dd47ee009c8dee0bdde9cee10fbe07b974fadb
Elisha Ukpong <ishukpong418@gmail.com>
2025-06-13 00:15:01 +0100

    fixed a function for unique words

--- src/modules/import.hs
+++ src/modules/import.hs
@@ -4,3 +4,9 @@ import Data.List
 
 numUniques :: (Eq a) => [a]-> Int
 numUniques = length . nub
+
+
+countWordsInString = length . words
+
+countOfUniqueWords ::  String -> Int
+countOfUniqueWords = length . group . sort . words
