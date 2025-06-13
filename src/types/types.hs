415a8b829215f80477c0d832569d5707c09446ce
Elisha Ukpong <ishukpong418@gmail.com>
2025-06-13 23:52:07 +0100

    haskell learnings

--- src/types/types.hs
+++ src/types/types.hs
@@ -1,2 +1,20 @@
-modules MyTypes where
+module MyTypes where
 
+data Bool = True | False
+
+data Point = Point Float Float deriving Show
+
+data Shape = Circle Point Float | Rectangle Point Point
+                deriving Show
+
+area (Circle _  r ) = pi * (r ^ 2);
+
+data Person = Person String String Int Float String deriving Show
+
+data PersonUsingRecord = PersonRecord {
+                                firstName :: String,
+                                lastName :: String,
+                                age :: Int,
+                                height :: Float,
+                                iceCream :: String
+                                } deriving Show
